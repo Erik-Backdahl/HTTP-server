bde82f0f4346ff8e18347959c96814b8b34c8b1d
Erik Bäckdahl <erik.backdahl@gmail.com>
2025-04-14 18:14:35 +0200

    [any message]

--- src/Server.cs
+++ src/Server.cs
@@ -7,4 +7,6 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+socket.Send(System.Text.Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+
