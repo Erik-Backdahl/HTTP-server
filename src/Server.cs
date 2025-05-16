56093f7742e5489e7e604d822bcd215525e26b0d
Erik Bäckdahl <erik.backdahl@gmail.com>
2025-05-16 14:55:12 +0200

    Added start of HTTP path detection

--- src/Server.cs
+++ src/Server.cs
@@ -29,35 +29,68 @@ namespace Application
                 await handleRequest(tcpclient);
                 //await sendResponse(tcpclient);
             }
-
-
-            Console.WriteLine("lol");
         }
 
         static async Task handleRequest(TcpClient client)
         {
-            await using NetworkStream ns = client.GetStream();
+            await using NetworkStream networkStream = client.GetStream();
+            using var reader = new StreamReader(networkStream, Encoding.UTF8, leaveOpen: true);
 
+            string? requestLine = await reader.ReadLineAsync();
+            if (string.IsNullOrEmpty(requestLine))
+            {
+                // Malformed request, return early or handle as needed
+                return;
+            }
+
+            // Example: "GET /path HTTP/1.1"
+            string[] requestParts = requestLine.Split(' ');
+            string requestType = requestParts[0];
+            string path = requestParts[1];
+
+            if (requestParts.Length < 2)
+            {
+                var failed = formatResponse("", "400");
+                var failedBytes = Encoding.UTF8.GetBytes(failed);
+
+                await networkStream.WriteAsync(failedBytes, 0, failedBytes.Length);
+                // Malformed request, return early or handle as needed
+                return;
+            }
+            LookForEndPoint(path);
+
+            //DeterminePath of Request
+
+
+
+
+
+
+
+
+            //Default Response
             var message = $"time {DateTime.Now}";
 
-
-            var responseTemplate = formatResponse(message);
+            var responseTemplate = formatResponse("Default " + message);
 
             var responseBytes = Encoding.UTF8.GetBytes(responseTemplate);
-            await ns.WriteAsync(responseBytes, 0, responseBytes.Length);
-
-
-            string secondMessage = "number 2";
-
-            responseTemplate = formatResponse(secondMessage);
-            responseBytes = Encoding.UTF8.GetBytes(responseTemplate);
-            await ns.WriteAsync(responseBytes, 0, responseBytes.Length);
-
-            await ns.FlushAsync();
+            await networkStream.WriteAsync(responseBytes, 0, responseBytes.Length);
         }
-        static string formatResponse(string response)
+
+        static string formatResponse(string response = "", string requestType = "200")
         {
-            var responseTemplate = $"HTTP/1.1 200 OK\r\n" +
+            string responseCode;
+
+            if  (requestType == "200")
+                responseCode = "HTTP/1.1 200 OK";
+            else if (requestType == "400")
+                responseCode = "HTTP/1.1 400 Bad Request";
+            else
+                responseCode = "HTTP/1.1 404 Not Found";
+
+
+
+            var responseTemplate = $"{responseCode}\r\n" +
             $"Content-Type: text/plain\r\n" +
             $"Content-Length: {response.Length}\r\n" +
             $"\r\n" +
@@ -66,10 +99,5 @@ namespace Application
             return responseTemplate;
 
         }
-        /*
-        static async Task sendResponse(TcpClient client)
-        {
-            
-        }*/
     }
 }
